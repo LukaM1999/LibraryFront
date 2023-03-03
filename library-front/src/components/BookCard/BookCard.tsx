6ec61f941c7b94eccbd6f08a30fca05769f4a546
Luka Miletić <miletic.ra194.2018@uns.ac.rs>
2023-03-03 16:48:05 +0100

    basic home page with navbar, footer and book card layout

--- library-front/src/components/BookCard/BookCard.tsx
+++ library-front/src/components/BookCard/BookCard.tsx
@@ -1,8 +1,12 @@
-import React, { FC } from 'react'
-import './BookCard.css'
+import React, { FC } from 'react';
+import './BookCard.css';
 
 interface BookCardProps {}
 
-const BookCard: FC<BookCardProps> = () => <div className='book-card'>BookCard Component</div>
+const BookCard: FC<BookCardProps> = () => (
+  <div className="BookCard">
+    BookCard Component
+  </div>
+);
 
-export default BookCard
+export default BookCard;

7c0eb1646efff0a174815ff17d1a5c7dc33052eb
Luka Miletić <miletic.ra194.2018@uns.ac.rs>
2023-03-06 15:55:54 +0100

    moved homepage to app component

--- library-front/src/main.tsx
+++ library-front/src/main.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import HomePage from './components/pages/HomePage/HomePage'
+import App from './App'
 import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )

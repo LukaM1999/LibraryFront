8eaae85c02dc2ca63f70a1ab636a7b6693403c6e
LukaM1999 <miletic.ra194.2018@uns.ac.rs>
2023-03-08 03:21:26 +0100

    burger button, navigation buttons in sidebar

--- library-front/src/components/Navbar/Navbar.tsx
+++ library-front/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,23 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
+import NavButton from '../NavButton/NavButton'
+import { AiFillHome } from 'react-icons/ai'
+import { FaUserAlt } from 'react-icons/fa'
 import './Navbar.css'
 
 interface NavbarProps {}
 
-const Navbar: FC<NavbarProps> = () => <div className='navbar'></div>
+const Navbar: FC<NavbarProps> = () => (
+  <>
+    <label className='hamburger-menu'>
+      <input type='checkbox' />
+    </label>
+    <aside className='sidebar'>
+      <nav>
+        <NavButton to='/' icon={<AiFillHome />} text='Home' />
+        <NavButton to='/profile' icon={<FaUserAlt />} text='Profile' />
+      </nav>
+    </aside>
+  </>
+)
 
 export default Navbar
